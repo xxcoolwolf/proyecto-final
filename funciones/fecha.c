4ea03c6a4627b9bd6e262e7470fd0bfe2d99e956
Mauricio Aquino <aquinomauricio34@gmail.com>
2021-11-06 17:00:14 -0300

    fjf

--- funciones/fecha.c
+++ funciones/fecha.c
@@ -1,8 +1,14 @@
 #ifndef FEHCA_C
 #define FECHA_C
 //prueba GITHUB---------------------------------------
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
+// time_t t;
+// t=time(NULL);
+// struct tm tiempoLocal = *localtime(&t);
+// printf("\n---dia = %d mes = %d anio = %d---\n",tiempoLocal.tm_mday,tiempoLocal.tm_mon+1,tiempoLocal.tm_year+1900);
 #endif //FECHA_C
